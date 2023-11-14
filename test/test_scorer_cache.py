e84afd006c22144cf259c579baacc5213606233c
Sean MacAvaney <sean.macavaney@gmail.com>
2023-11-14 15:27:28 -0500

    test wip

--- test/test_scorer_cache.py
+++ test/test_scorer_cache.py
@@ -97,40 +97,4 @@ class TestScorerCache(unittest.TestCase):
             res = cache(pd.DataFrame([
                 {'qid': 'a', 'query': 'a', 'docno': '1'},
             ]))
-            import pdb; pdb.set_trace()
-            str(cache.path)
-            # self.assertTrue((res == pd.DataFrame([
-            #     {'qid': 'a', 'query': 'a', 'docno': '1', 'score': 1., 'rank': 0},
-            #     {'qid': 'a', 'query': 'a', 'docno': '2', 'score': 1., 'rank': 1},
-            #     {'qid': 'b', 'query': 'b', 'docno': '2', 'score': 1., 'rank': 0},
-            # ])).all().all())
-
-            # with self.subTest('make sure it doesn\'t call the scorer again on any of these'):
-            #     cache.scorer = scorer_raises
-            #     res = cache(pd.DataFrame([
-            #         {'qid': 'a', 'query': 'a', 'docno': '1'},
-            #         {'qid': 'a', 'query': 'a', 'docno': '2'},
-            #         {'qid': 'b', 'query': 'b', 'docno': '2'},
-            #     ]))
-            #     self.assertTrue((res == pd.DataFrame([
-            #         {'qid': 'a', 'query': 'a', 'docno': '1', 'score': 1., 'rank': 0},
-            #         {'qid': 'a', 'query': 'a', 'docno': '2', 'score': 1., 'rank': 1},
-            #         {'qid': 'b', 'query': 'b', 'docno': '2', 'score': 1., 'rank': 0},
-            #     ])).all().all())
-
-            # with self.subTest('make sure it calls a new scorer on any unknown values'):
-            #     cache.scorer = scorer_2
-            #     res = cache(pd.DataFrame([
-            #         {'qid': 'a', 'query': 'a', 'docno': '1'},
-            #         {'qid': 'a', 'query': 'a', 'docno': '3'},
-            #         {'qid': 'b', 'query': 'b', 'docno': '2'},
-            #         {'qid': 'b', 'query': 'a', 'docno': '1'}, # should pull from (a,1) cache (not dependent on qid)
-            #         {'qid': 'c', 'query': 'c', 'docno': '1'},
-            #     ]))
-            #     self.assertTrue((res == pd.DataFrame([
-            #         {'qid': 'a', 'query': 'a', 'docno': '1', 'score': 1., 'rank': 1}, # old
-            #         {'qid': 'a', 'query': 'a', 'docno': '3', 'score': 2., 'rank': 0}, # new
-            #         {'qid': 'b', 'query': 'a', 'docno': '1', 'score': 1., 'rank': 0}, # old
-            #         {'qid': 'b', 'query': 'b', 'docno': '2', 'score': 1., 'rank': 1}, # old
-            #         {'qid': 'c', 'query': 'c', 'docno': '1', 'score': 2., 'rank': 0}, # new
-            #     ])).all().all())
+            # TODO: what to test? File size?
