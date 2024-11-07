1afa3269960b1a9618d5b012fed175c32e026116
Sean MacAvaney <sean.macavaney@gmail.com>
2024-11-07 22:48:06 +0000

    sqlite3 is faster than dbm.sqlite3

--- examples/scorer_monot5.py
+++ examples/scorer_monot5.py
@@ -8,15 +8,16 @@ dataset = pt.get_dataset('irds:msmarco-passage/trec-dl-2019/judged')
 bm25 = pt.terrier.Retriever.from_dataset('msmarco_passage', 'terrier_stemmed', wmodel='BM25')
 monot5 = pt.text.get_text(dataset, 'text') >> MonoT5ReRanker()
 cached_monot5 = SparseScorerCache('monot5.cache', monot5, verbose=True)
-pipeline = bm25 >> cached_monot5
+inp = bm25(dataset.get_topics())
+pipeline = cached_monot5
 
 
 t0 = time()
-pipeline(dataset.get_topics())
+pipeline(inp)
 t1 = time()
 print(f'first invocation: {t1-t0:.3f}s')
 
 t0 = time()
-pipeline(dataset.get_topics())
+pipeline(inp)
 t1 = time()
 print(f'second invocation: {t1-t0:.3f}s')
--- pyterrier_caching/__init__.py
+++ pyterrier_caching/__init__.py
@@ -1,8 +1,7 @@
 __version__ = '0.2.0'
 
-from pyterrier_caching import dbm_sqlite3
 from pyterrier_caching.util import Lazy, closing_memmap, meta_file_compat
 from pyterrier_caching.builder import artifact_builder, BuilderMode
 from pyterrier_caching.indexer_cache import IndexerCache, Lz4PickleIndexerCache
-from pyterrier_caching.scorer_cache import ScorerCache, DenseScorerCache, SparseScorerCache, Hdf5ScorerCache, DbmSqlite3ScorerCache, Sqlite3ScorerCache
+from pyterrier_caching.scorer_cache import ScorerCache, DenseScorerCache, SparseScorerCache, Hdf5ScorerCache, Sqlite3ScorerCache
 from pyterrier_caching.retriever_cache import RetrieverCache, DbmRetrieverCache
