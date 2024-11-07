8d72ce940e7a9f0f045ee9ebc4c91faab6e05a93
Sean MacAvaney <sean.macavaney@gmail.com>
2024-11-07 19:44:54 +0000

    documentation and example

--- examples/scorer_monot5.py
+++ examples/scorer_monot5.py
@@ -1,16 +1,13 @@
 from time import time
 import pyterrier as pt
-pt.init()
-from pyterrier_caching import ScorerCache
+from pyterrier_caching import SparseScorerCache
 from pyterrier_t5 import MonoT5ReRanker
 
 # Setup
 dataset = pt.get_dataset('irds:msmarco-passage/trec-dl-2019/judged')
-bm25 = pt.BatchRetrieve.from_dataset('msmarco_passage', 'terrier_stemmed', wmodel='BM25')
+bm25 = pt.terrier.Retriever.from_dataset('msmarco_passage', 'terrier_stemmed', wmodel='BM25')
 monot5 = pt.text.get_text(dataset, 'text') >> MonoT5ReRanker()
-cached_monot5 = ScorerCache('monot5.cache', monot5, verbose=True)
-if not cached_monot5.built():
-    cached_monot5.build(dataset.get_corpus_iter())
+cached_monot5 = SparseScorerCache('monot5.cache', monot5, verbose=True)
 pipeline = bm25 >> cached_monot5
 
 
