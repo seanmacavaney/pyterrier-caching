cec2b7632f7fb0f97b8879a4a8e2f592e8ace45c
Sean MacAvaney <sean.macavaney@gmail.com>
2024-11-22 12:35:24 +0000

    a few enhancements to IndexerCache:  - a text_loader transformer  - path optional (if None, creates a temporary directory for the cache)

--- test/test_indexer_cache.py
+++ test/test_indexer_cache.py
@@ -1,6 +1,7 @@
 import tempfile
 import unittest
 from pathlib import Path
+import pandas as pd
 import pyterrier_caching
 
 class TestIndexerCache(unittest.TestCase):
@@ -46,6 +47,76 @@ class TestIndexerCache(unittest.TestCase):
                 ])
             with self.subTest('empty'):
                 self.assertEqual(list(cache.get_corpus_iter(start=3, stop=1)), [])
+            with self.subTest('text_loader'):
+                loader = cache.text_loader()
+                self.assertEqual(loader([{'docno': '1'}]), [
+                    {'docno': '1', 'data': 'test'},
+                ])
+                self.assertEqual(loader([{'docno': '2'}]), [
+                    {'docno': '2', 'data': 'caching pyterrier'},
+                ])
+                self.assertEqual(loader([{'docno': '2'}, {'docno': '5'}, {'docno': '2'}]), [
+                    {'docno': '2', 'data': 'caching pyterrier'},
+                    {'docno': '5', 'data': 'foo bar baz'},
+                    {'docno': '2', 'data': 'caching pyterrier'},
+                ])
+                self.assertEqual(len(loader(pd.DataFrame([], columns=['docno']))), 0)
+
+    def test_temp(self):
+        with pyterrier_caching.IndexerCache() as cache:
+            with self.subTest('len before built'):
+                with self.assertRaises(RuntimeError):
+                    len(cache)
+            cache.index([
+                {'docno': '1', 'data': 'test'},
+                {'docno': '2', 'data': 'caching pyterrier'},
+                {'docno': '3', 'data': 'fetch me some data'},
+                {'docno': '4', 'data': 'information retrieval'},
+                {'docno': '5', 'data': 'foo bar baz'},
+            ])
+            with self.subTest('len when built'):
+                self.assertEqual(len(cache), 5)
+            with self.subTest('full iter'):
+                self.assertEqual(list(cache), [
+                    {'docno': '1', 'data': 'test'},
+                    {'docno': '2', 'data': 'caching pyterrier'},
+                    {'docno': '3', 'data': 'fetch me some data'},
+                    {'docno': '4', 'data': 'information retrieval'},
+                    {'docno': '5', 'data': 'foo bar baz'},
+                ])
+            with self.subTest('start only'):
+                self.assertEqual(list(cache.get_corpus_iter(start=2)), [
+                    {'docno': '3', 'data': 'fetch me some data'},
+                    {'docno': '4', 'data': 'information retrieval'},
+                    {'docno': '5', 'data': 'foo bar baz'},
+                ])
+            with self.subTest('stop only'):
+                self.assertEqual(list(cache.get_corpus_iter(stop=2)), [
+                    {'docno': '1', 'data': 'test'},
+                    {'docno': '2', 'data': 'caching pyterrier'},
+                ])
+            with self.subTest('start and stop'):
+                self.assertEqual(list(cache.get_corpus_iter(start=1, stop=3)), [
+                    {'docno': '2', 'data': 'caching pyterrier'},
+                    {'docno': '3', 'data': 'fetch me some data'},
+                ])
+            with self.subTest('empty'):
+                self.assertEqual(list(cache.get_corpus_iter(start=3, stop=1)), [])
+            with self.subTest('text_loader'):
+                loader = cache.text_loader()
+                self.assertEqual(loader([{'docno': '1'}]), [
+                    {'docno': '1', 'data': 'test'},
+                ])
+                self.assertEqual(loader([{'docno': '2'}]), [
+                    {'docno': '2', 'data': 'caching pyterrier'},
+                ])
+                self.assertEqual(loader([{'docno': '2'}, {'docno': '5'}, {'docno': '2'}]), [
+                    {'docno': '2', 'data': 'caching pyterrier'},
+                    {'docno': '5', 'data': 'foo bar baz'},
+                    {'docno': '2', 'data': 'caching pyterrier'},
+                ])
+                self.assertEqual(len(loader(pd.DataFrame([], columns=['docno']))), 0)
+        self.assertFalse(cache.built())
 
     def test_empty(self):
         with tempfile.TemporaryDirectory() as d:
