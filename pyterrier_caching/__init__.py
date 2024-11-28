d75a0e5d2831db34c45f63ddca1261145e2c3861
Sean MacAvaney <sean.macavaney@gmail.com>
2024-11-28 09:22:08 +0000

    bump version

--- pyterrier_caching/__init__.py
+++ pyterrier_caching/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 from pyterrier_caching.util import Lazy, closing_memmap, meta_file_compat
 from pyterrier_caching.indexer_cache import IndexerCache, Lz4PickleIndexerCache
